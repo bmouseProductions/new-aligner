de2e5db9303b20b3002ad83b9dea55be05e23fe7
João Victor Corado <jhonisilva545@gmail.com>
2023-07-09 19:36:01 -0300

    concertando o slider

--- src/components/SectionSeis/index.tsx
+++ src/components/SectionSeis/index.tsx
@@ -25,7 +25,7 @@ export const SectionSeis = () => {
             clickable: true,
           }}
           modules={[Pagination]}
-          className="mySwiper hidden lg:block md:w-[80%] py-10 lg:w-[60%] "
+          className="mySwiper hidden lg:block py-10"
         >
           <SwiperSlide className="m-auto">
             <iframe
@@ -85,7 +85,7 @@ export const SectionSeis = () => {
             clickable: true,
           }}
           modules={[Pagination]}
-          className="mySwiper lg:hidden"
+          className="mySwiper py-10 lg:hidden"
         >
           <SwiperSlide className="m-auto">
             <iframe
