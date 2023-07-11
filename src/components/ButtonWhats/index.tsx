85b2426f30cf73947df0a9e561ac578b3aaa3fca
João Victor Corado <jhonisilva545@gmail.com>
2023-07-11 11:54:26 -0300

    button

--- src/components/ButtonWhats/index.tsx
+++ src/components/ButtonWhats/index.tsx
@@ -7,7 +7,7 @@ export const ButtonWhats = () => {
       href="https://api.whatsapp.com/send?phone=5513997289237&text=Gostaria%20de%20falar%20com%20um%20consultor"
       target="_blank"
     >
-      <BsWhatsapp className="text-white w-20px  md:w-[30px]"></BsWhatsapp>
+      <BsWhatsapp size={30} className="text-white  "></BsWhatsapp>
     </a>
   );
 };
