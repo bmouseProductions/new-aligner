9f4a4e00c2f8d5f6ed06443f738ce94e7dcabcfa
João Victor Corado <jhonisilva545@gmail.com>
2023-07-13 10:57:00 -0300

    video

--- src/components/SectionUm/index.tsx
+++ src/components/SectionUm/index.tsx
@@ -1,31 +1,6 @@
-import { useEffect, useState, useRef } from "react";
 import { Button } from "../Button";
 
 export const SectionUm = () => {
-  const [isIntersecting, setIsIntersecting] = useState(false);
-
-  const observerRef = useRef(null);
-
-  useEffect(() => {
-    const observerNode = observerRef.current;
-
-    const observerOptions = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.1,
-    };
-
-    const observer = new IntersectionObserver(([entry]) => {
-      setIsIntersecting(entry.isIntersecting);
-    }, observerOptions);
-
-    if (observerNode) observer.observe(observerNode);
-
-    return () => {
-      if (observerNode) observer.unobserve(observerNode);
-    };
-  }, []);
-
   return (
     <div className="bg-white bg-opacity-50 backdrop-blur-md  transition-all duration-300  text-black p-10  flex flex-col items-center gap-10  md:px-20 md:py-10 lg:justify-between lg:px-28 xl:px-52 xl:py-20">
       <h1 className=" text-black text-center text-4xl font-bold   md:text-5xl xl:text-6xl  2xl:text-7xl">
@@ -33,15 +8,11 @@ export const SectionUm = () => {
         mais com o <span className="text-[#004fab]">New Aligner</span>
       </h1>
 
-      <div ref={observerRef}>
-        {isIntersecting && (
-          <iframe
-            className="shadow-2xl border-8 border-[#004fab] shadow-[#004fab] rounded-3xl md:w-[560px] md:h-[315px]  2xl:w-[800px] 2xl:h-[420px]"
-            src="https://www.youtube.com/embed/CusX3r5Lgl0"
-            title="YouTube video player"
-          ></iframe>
-        )}
-      </div>
+      <iframe
+        className="shadow-2xl border-8 border-[#004fab] shadow-[#004fab] rounded-3xl md:w-[560px] md:h-[315px]  2xl:w-[800px] 2xl:h-[420px]"
+        src="https://www.youtube.com/embed/CusX3r5Lgl0"
+        title="YouTube video player"
+      ></iframe>
       <Button></Button>
     </div>
   );
